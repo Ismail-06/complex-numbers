53267675877dd5ac4b987c90a8e3a85ec6caa6b4
Ismail <assasin.creed.1999@bk.ru>
2021-03-03 12:09:51 +0300

    complex numbrers

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
 <?php
-echo 'hello';
+class MathClass {
+    public $num1;
+    public $num2;
+    public function __construct($x1, $x2)
+    {
+        $this->num1 = $x1;
+        $this->num2 = $x2;
+    }
+
+    public function sumNum(){
+        return $this->num1 + $this->num2;
+    }
+
+    public function subNum(){
+        return $this->num1 - $this->num2;
+    }
+    public function divNum(){
+        return $this->num1 * $this->num2;
+    }
+    public function multiNum(){
+        return $this->num1 / $this->num2;
+    }
+}
+
+$obj = new MathClass(2,5);
+echo $obj->sumNum();
+echo $obj->subNum();
+echo $obj->divNum();
+echo $obj->multiNum();
+
+
